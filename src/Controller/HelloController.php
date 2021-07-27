91b180c7387602a97ca87ef1050cad5fb0a96b03
chokri-91 <chokri.hmida91@gmail.com>
2021-07-27 15:04:02 +0200

    creation route hello

---
+++ src/Controller/HelloController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HelloController extends AbstractController
+{
+    #[Route('/hello', name: 'hello')]
+    
+    public function index(string $firstName = 'Chokri',int $age = 20): Response
+    {
+        if($age < 18)
+        {
+            return $this->redirect('https://www.disney.fr');
+        }        
+        $firstName = ucfirst($firstName);
+
+        return new Response("<p>Bonjour $firstName, vous avez $age ans</p>");
+    }
+}
